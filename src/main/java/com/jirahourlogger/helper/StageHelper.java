a9c40b72f581b2e8662688acdc50ec86c09cda5e
Acdaling Edusei <acdaling@gmail.com>
2026-05-18 22:04:01 +0200

    Created stageHelper
    - Helper that we use to interface with the Stage component for JavaFX

---
+++ src/main/java/com/jirahourlogger/helper/StageHelper.java
@@ -0,0 +1,54 @@
+package com.jirahourlogger.helper;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class StageHelper {
+    private final Stage stage;
+
+    public StageHelper() {
+        this.stage = new Stage();
+    }
+
+    public StageHelper initStyle(StageStyle style) {
+        stage.initStyle(style);
+        return this;
+    }
+
+    public StageHelper setAlwaysOnTop(boolean alwaysOnTop) {
+        stage.setAlwaysOnTop(alwaysOnTop);
+        return this;
+    }
+
+    public StageHelper setScene(Scene scene) {
+        stage.setScene(scene);
+        return this;
+    }
+
+    public StageHelper setPosition(double x, double y) {
+        stage.setX(x);
+        stage.setY(y);
+        return this;
+    }
+
+    public double getX() {
+        return stage.getX();
+    }
+
+    public double getY() {
+        return stage.getY();
+    }
+
+    public void setX(double x) {
+        stage.setX(x);
+    }
+
+    public void setY(double y) {
+        stage.setY(y);
+    }
+
+    public void show() {
+        stage.show();
+    }
+}
